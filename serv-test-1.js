0bd372a25283f835385dda124443c018a0352fc4
JasonVonKrueger <rpoore@gmail.com>
2016-08-01 20:52:59 -0400

    Initial commit

---
+++ serv-test-1.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var url = require('url');
+var mysql = require ('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '',
+  password : '',
+  database : 'orb'
+});
+
+
+//var post  = {id: 1, title: 'Hello MySQL'};
+//var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
+  // Neat!
+//});
+//console.log(query.sql);
+
+
+
+
+httpServer = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World\n');
+
+  var params = url.parse(req.url,true).query;
+  console.log(params);
+
+    //var a = params.number1;
+    //var b = params.number2;
+
+    //var numA = new Number(a);
+    //var numB = new Number(b);
+
+    //var numOutput = new Number(numA + numB).toFixed(0);
+
+    //res.write(numOutput);
+    //res.end();
+
+});
+
+httpServer.listen(1337, "192.168.1.68");
+
+console.log('Server running at http://192.168.1.68:1337/');
+
+connection.connect();       
+
+var query = connection.query('SELECT * FROM parents LIMIT 3', function (error, rows, fields) {
+  //res.write(rows[1].COUNTRY);
+  console.log(rows[1].COUNTRY);
+});
+
+connection.end();
